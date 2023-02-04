b1940933d038e7c14f2292a97914b45f72eb6f56
Gorhin Stroebel <102311306+gorhinstroebel@users.noreply.github.com>
2023-02-04 14:54:55 +0200

    Update Ping.pas
    Format Code

--- Ping.pas
+++ Ping.pas
@@ -19,59 +19,59 @@ ReplyBuffer: Pointer; ReplySize: DWORD; Timeout: DWORD): DWORD; stdcall;
 external 'iphlpapi.dll';
 
 type
-TEchoReply = packed record
-Addr: In_Addr;
-Status: DWORD;
-RoundTripTime: DWORD;
+  TEchoReply = packed record
+  Addr: In_Addr;
+  Status: DWORD;
+  RoundTripTime: DWORD;
 end;
 
 PEchoReply = ^TEchoReply;
 
 var
-WSAData: TWSAData;
+  WSAData: TWSAData;
 
 procedure Startup;
 begin
-if WSAStartup(MAKEWORD(2, 2), WSAData) <> 0 then
-raise Exception.Create('WSAStartup');
+  if WSAStartup(MAKEWORD(2, 2), WSAData) <> 0 then
+  raise Exception.Create('WSAStartup');
 end;
 
 procedure Cleanup;
 begin
-if WSACleanup <> 0 then
-raise Exception.Create('WSACleanup');
+  if WSACleanup <> 0 then
+  raise Exception.Create('WSACleanup');
 end;
 
 function PingHost(const HostName: AnsiString; TimeoutMS: cardinal = 900): boolean;
 const
-rSize = $400;
+  rSize = $400;
 var
-h: THandle;
-d: string;
-r: array [0 .. rSize - 1] of byte;
-i: cardinal;
-Addr: TSockAddrIn;
-AddrInfo: PHostEnt;
+  h: THandle;
+  d: string;
+  r: array [0 .. rSize - 1] of byte;
+  i: cardinal;
+  Addr: TSockAddrIn;
+  AddrInfo: PHostEnt;
 begin
-Startup;
-AddrInfo := gethostbyname(PAnsiChar(HostName));
-if AddrInfo = nil then
-RaiseLastOSError;
-try
-h := IcmpCreateFile;
-if h = INVALID_HANDLE_VALUE then
-RaiseLastOSError;
-try
-d := FormatDateTime('yyyymmddhhnnsszzz', Now);
-Addr.sin_addr.s_addr := PInAddr(AddrInfo^.h_addr_list^)^.S_addr;
-i := IcmpSendEcho(h, Addr.sin_addr, PChar(d), Length(d), nil, @r[0], rSize, TimeoutMS);
-Result := (i <> 0) and (PEchoReply(@r[0]).Status = 0);
-finally
-IcmpCloseHandle(h);
-end;
-finally
-Cleanup;
-end;
+  Startup;
+  AddrInfo := gethostbyname(PAnsiChar(HostName));
+  if AddrInfo = nil then
+  RaiseLastOSError;
+  try
+  h := IcmpCreateFile;
+  if h = INVALID_HANDLE_VALUE then
+  RaiseLastOSError;
+ try
+  d := FormatDateTime('yyyymmddhhnnsszzz', Now);
+  Addr.sin_addr.s_addr := PInAddr(AddrInfo^.h_addr_list^)^.S_addr;
+  i := IcmpSendEcho(h, Addr.sin_addr, PChar(d), Length(d), nil, @r[0], rSize, TimeoutMS);
+  Result := (i <> 0) and (PEchoReply(@r[0]).Status = 0);
+  finally
+    IcmpCloseHandle(h);
+  end;
+   finally
+    Cleanup;
+  end;
 end;
 
 end.
