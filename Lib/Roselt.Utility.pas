c86bc837069651265cb6a4a3c2c872ca3bbaec52
Shaun Roselt <shaunroselt@gmail.com>
2023-09-27 02:01:53 +0200

    Added support for TMS Web Core

--- Lib/Roselt.Utility.pas
+++ Lib/Roselt.Utility.pas
@@ -1,3 +1,4 @@
+//  Helper functions used within Roselt Developer Tools
 unit Roselt.Utility;
 
 interface
@@ -25,6 +26,11 @@ uses
   System.Classes;
 
 
+{$IFDEF WEBLIB}
+// The WEBLIB functions are only used in the Web Version of the app
+procedure SetQueryParam(key, value: String; url: String = '');
+{$IFEND}
+
 procedure OpenURL(URL: string);
 
 function RemoveNonDigits(const S: string): string;
@@ -33,6 +39,33 @@ function RemoveNonDigitsAndLetters(const S: string): string;
 function RemoveEmptyLinesAndWhitespace(const S: string): string;
 
 implementation
+
+{$IFDEF WEBLIB}
+procedure SetQueryParam(key, value, url: String);
+begin
+  asm
+    if (!url) url = window.location.href;
+    let re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi"),hash;
+
+    if (re.test(url))
+        if (typeof value !== 'undefined' && value !== null)
+            url = url.replace(re, '$1' + key + "=" + value + '$2$3');
+        else {
+            hash = url.split('#');
+            url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) url += '#' + hash[1];
+        }
+    else
+        if (typeof value !== 'undefined' && value !== null) {
+            const separator = url.indexOf('?') !== -1 ? '&' : '?';
+            hash = url.split('#');
+            url = hash[0] + separator + key + '=' + value;
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) url += '#' + hash[1];
+        }
+    window.history.pushState({ path: url }, '', url);
+  end;
+end;
+{$IFEND}
 
 procedure OpenURL(URL: string);
 begin
@@ -51,43 +84,65 @@ begin
   {$IFDEF IOS}
     SharedApplication.OpenURL(StrToNSUrl(URL));
   {$ENDIF}
+  {$IFDEF WEBLIB}
+    asm
+      window.open(URL, '_blank').focus();
+    end;
+  {$ENDIF}
 end;
 
 function RemoveNonDigits(const S: string): string;
 begin
-  SetLength(Result, S.Length);
-  var LActualLength := 0;
-  for var i := 1 to S.Length do
-    if CharInSet(S[i],  ['0'..'9']) then
-    begin
-      Inc(LActualLength);
-      Result[LActualLength] := S[i];
+  {$IFNDEF WEBLIB}
+    SetLength(Result, S.Length);
+    var LActualLength := 0;
+    for var I := 1 to S.Length do
+      if CharInSet(S[I],  ['0'..'9']) then
+      begin
+        Inc(LActualLength);
+        Result[LActualLength] := S[I];
+      end;
+    SetLength(Result, LActualLength);
+  {$ENDIF}
+  {$IFDEF WEBLIB}
+    asm
+      return S.replace(/[^0-9]/g,'');
     end;
-  SetLength(Result, LActualLength);
+  {$ENDIF}
 end;
 
 function RemoveNonDigitsAndLetters(const S: string): string;
 begin
-  SetLength(Result, S.Length);
-  var LActualLength := 0;
-  for var i := 1 to S.Length do
-    if CharInSet(S[i],  ['0'..'9']) OR CharInSet(S[i],  ['A'..'Z']) OR CharInSet(S[i],  ['a'..'z']) then
-    begin
-      Inc(LActualLength);
-      Result[LActualLength] := S[i];
+  {$IFNDEF WEBLIB}
+    SetLength(Result, S.Length);
+    var LActualLength := 0;
+    for var I := 1 to S.Length do
+      if CharInSet(S[I],  ['0'..'9']) OR CharInSet(S[I],  ['A'..'Z']) OR CharInSet(S[I],  ['a'..'z']) then
+      begin
+        Inc(LActualLength);
+        Result[LActualLength] := S[I];
+      end;
+    SetLength(Result, LActualLength);
+  {$ENDIF}
+  {$IFDEF WEBLIB}
+    asm
+      return S.replace(/[^a-zA-Z0-9]/g,'');
     end;
-  SetLength(Result, LActualLength);
+  {$ENDIF}
 end;
 
 function RemoveEmptyLinesAndWhitespace(const S: string): string;
+var
+  SL: TStringList;
+  I: UInt64;
 begin
-  var SL := TStringList.Create;
+  SL := TStringList.Create;
   SL.Text := S;
 
-  for var i := SL.Count - 1 downto 0 do
+  for I := SL.Count - 1 downto 0 do
   begin
-    SL[i] := Trim(SL[i]);
-    if SL[i] = '' then SL.Delete(i);
+    SL[I] := Trim(SL[I]);
+    if SL[I] = '' then SL.Delete(I);
   end;
 
   Result := SL.Text;
