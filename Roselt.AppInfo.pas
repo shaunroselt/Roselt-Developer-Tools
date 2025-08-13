2608dd0fcc957f37697605ac80d4e1c5624710f2
Shaun Roselt <Me@ShaunRoselt.com>
2025-08-13 22:07:20 +0200

    Improved AppInfo

--- Roselt.AppInfo.pas
+++ Roselt.AppInfo.pas
@@ -2,14 +2,16 @@ unit Roselt.AppInfo;
 
 interface
 
-uses System.SysUtils;
+uses
+  System.SysUtils,
+  System.IOUtils;
 
 function GetAppInfo: String; // Need to get all of the App Info dynamically in the future
 
 implementation
 
 function GetAppInfo: String;
-  function GetAppArchitectureInfo: String;
+  function GetAppArchitectureInfo: String; // Consider using TOSVersion.Architecture
   begin
     Result := 'Unknown Architecture';
     {$IFDEF MSWINDOWS} Result := 'X64'; {$ENDIF}
@@ -19,18 +21,32 @@ function GetAppInfo: String;
     {$IFDEF ANDROID} Result := 'X64'; {$ENDIF}
     {$IFDEF WEBLIB} Result := 'Web'; {$ENDIF}
   end;
+  function GetAppCompiledDate: String;
+  begin
+    try
+      Result := DateToStr(TFile.GetLastWriteTime(ParamStr(0)));
+    except
+      Result := DateToStr(Date);
+    end;
+  end;
 var
-  Version, Architecture, BuildType, CompiledDate: String;
+  Version, OperatingSystem, Architecture, BuildType, CompiledDate: String;
 begin
-  Version := 'Version 3.0.0.0 Beta 4';
+  Version := 'Version 3.0.0.0 Beta 5';
   Architecture := GetAppArchitectureInfo;
+  OperatingSystem := TOSVersion.Name;
   BuildType := 'RELEASE';
   {$IFDEF DEBUG}
     BuildType := 'DEBUG';
   {$ENDIF}
-  CompiledDate := DateToStr(Date);
+  CompiledDate := GetAppCompiledDate;
 
-  Result := Version + ' | ' + Architecture + ' | ' + BuildType + ' | ' + CompiledDate;
+  Result := '${Version} | ${OperatingSystem} | ${Architecture} | ${BuildType} | ${CompiledDate}';
+  Result := Result.Replace('${Version}', Version)
+                  .Replace('${OperatingSystem}', OperatingSystem)
+                  .Replace('${Architecture}', Architecture)
+                  .Replace('${BuildType}', BuildType)
+                  .Replace('${CompiledDate}', CompiledDate);
 end;
 
 end.
